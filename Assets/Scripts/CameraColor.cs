65a1e5971e6a5c39ae98b285e86195c339cdb40a
Daniel <danjfrank08@gmail.com>
2019-06-09 00:01:20 -0500

    HomeScreen UI
    Background color interpolates between two colors
    Keeping text constant colors for now

---
+++ Assets/Scripts/CameraColor.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace ShapesAndColors{
+    public class CameraColor : MonoBehaviour
+    {
+        Camera mainCam;
+        List<Color> colorList = new List<Color>(){
+            Color.magenta,
+            Color.cyan
+        };
+
+        void Awake()
+        {
+            mainCam = GetComponent<Camera>();
+            mainCam.clearFlags = CameraClearFlags.SolidColor;
+        }
+
+        void Update()
+        {
+            float duration = 3f;
+            float t = Mathf.PingPong(Time.time, duration) / duration;
+            mainCam.backgroundColor = Color.Lerp(colorList[0], colorList[1], t);
+        }
+    }
+}
--- Assets/Scripts/TextController.cs
+++ Assets/Scripts/TextController.cs
@@ -1,17 +1,23 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityStandardAssets;
 using UnityEngine.UI;
 
-public class TextController : MonoBehaviour
-{
-
-    void Awake()
+namespace ShapesAndColors{
+    public class TextController : MonoBehaviour
     {
-        Font arial;
-        arial = (Font)Resources.GetBuiltinResource(typeof(Font), "Arial.ttf");
+        public Font font;
+        private Color GetshapesTextColor{ get; set; }
+        private Color GetampTextColor{ get; set; }
+        private Color GetcolorsTextColor{ get; set; }
+        
+        void Awake()
+        {
+            var welcomeFont = font;
+            var welcomeFontSize = 100;
 
-        //Create the UI Canvas
+            //Create the UI Canvas
             GameObject canvasGO = new GameObject();
             canvasGO.name = "Canvas";
             canvasGO.AddComponent<Canvas>();
@@ -22,26 +28,26 @@ public class TextController : MonoBehaviour
             canvas = canvasGO.GetComponent<Canvas>();
             canvas.renderMode = RenderMode.ScreenSpaceOverlay;
 
-        //Create the 'Welcome to' text
+            //Create the 'Welcome to' text
             GameObject welcomeTextGO = new GameObject();
             welcomeTextGO.name = "WelcomeText";
             welcomeTextGO.transform.parent = canvasGO.transform;
             welcomeTextGO.AddComponent<Text>();
-            
+
             Text welcomeText;
             welcomeText = welcomeTextGO.GetComponent<Text>();
             welcomeText.text = "Welcome to";
-            welcomeText.font = arial;
-            welcomeText.fontSize = 60;
+            welcomeText.font = welcomeFont;
+            welcomeText.fontSize = welcomeFontSize;
             welcomeText.color = new Color(255, 0, 0);
             welcomeText.alignment = TextAnchor.UpperCenter;
 
             RectTransform welcomeRectTransform;
             welcomeRectTransform = welcomeText.GetComponent<RectTransform>();
-            welcomeRectTransform.localPosition = new Vector3(0,300,0);
-            welcomeRectTransform.sizeDelta = new Vector2(600,200);
+            welcomeRectTransform.localPosition = new Vector3(0, 300, 0);
+            welcomeRectTransform.sizeDelta = new Vector2(600, 200);
 
-        //Create the 'Shapes' text
+            //Create the 'Shapes' text
             GameObject shapesTextGO = new GameObject();
             shapesTextGO.name = "ShapesText";
             shapesTextGO.transform.parent = canvasGO.transform;
@@ -50,17 +56,17 @@ public class TextController : MonoBehaviour
             Text shapesText;
             shapesText = shapesTextGO.GetComponent<Text>();
             shapesText.text = "Shapes";
-            shapesText.font = arial;
-            shapesText.fontSize = 65;
+            shapesText.font = welcomeFont;
+            shapesText.fontSize = welcomeFontSize + 5;
             shapesText.color = new Color(0, 255, 0);
             shapesText.alignment = TextAnchor.UpperLeft;
 
             RectTransform shapesRectTransform;
             shapesRectTransform = shapesText.GetComponent<RectTransform>();
-            shapesRectTransform.localPosition = new Vector3(-1,250,0);
-            shapesRectTransform.sizeDelta = new Vector2(600,200);
+            shapesRectTransform.localPosition = new Vector3(-183.5f, 200f, 0f);
+            shapesRectTransform.sizeDelta = new Vector2(600, 200);
 
-        //Create the '&' text
+            //Create the '&' text
             GameObject ampTextGO = new GameObject();
             ampTextGO.name = "AmpersandText";
             ampTextGO.transform.parent = canvasGO.transform;
@@ -69,17 +75,17 @@ public class TextController : MonoBehaviour
             Text ampText;
             ampText = ampTextGO.GetComponent<Text>();
             ampText.text = "&";
-            ampText.font = arial;
-            ampText.fontSize = 65;
+            ampText.font = welcomeFont;
+            ampText.fontSize = welcomeFontSize + 5;
             ampText.color = new Color(0, 0, 255);
             ampText.alignment = TextAnchor.UpperCenter;
 
             RectTransform ampRectTransform;
             ampRectTransform = ampText.GetComponent<RectTransform>();
-            ampRectTransform.localPosition = new Vector3(0,250,0);
-            ampRectTransform.sizeDelta = new Vector2(600,200);
+            ampRectTransform.localPosition = new Vector3(0, 200, 0);
+            ampRectTransform.sizeDelta = new Vector2(600, 200);
 
-        //Create the 'Colors!' text
+            //Create the 'Colors!' text
             GameObject colorsTextGO = new GameObject();
             colorsTextGO.name = "ColorsText";
             colorsTextGO.transform.parent = canvasGO.transform;
@@ -88,17 +94,17 @@ public class TextController : MonoBehaviour
             Text colorsText;
             colorsText = colorsTextGO.GetComponent<Text>();
             colorsText.text = "Colors!";
-            colorsText.font = arial;
-            colorsText.fontSize = 65;
+            colorsText.font = welcomeFont;
+            colorsText.fontSize = welcomeFontSize + 5;
             colorsText.color = new Color(255, 255, 0);
             colorsText.alignment = TextAnchor.UpperRight;
 
             RectTransform colorsRectTransform;
             colorsRectTransform = colorsText.GetComponent<RectTransform>();
-            colorsRectTransform.localPosition = new Vector3(0,250,1);
-            colorsRectTransform.sizeDelta = new Vector2(600,200);
+            colorsRectTransform.localPosition = new Vector3(172, 200, 1);
+            colorsRectTransform.sizeDelta = new Vector2(600, 200);
 
-        //Create the 'Start!' button
+            //Create the 'Start!' button
             GameObject startButtonGO = new GameObject();
             startButtonGO.name = "StartButton";
             startButtonGO.transform.parent = canvasGO.transform;
@@ -109,13 +115,13 @@ public class TextController : MonoBehaviour
             RectTransform startButtonRectTransform;
             startButtonRectTransform = startButtonGO.GetComponent<RectTransform>();
             startButtonRectTransform.localPosition = new Vector3(0, -1, 0);
-            startButtonRectTransform.sizeDelta = new Vector2(300,100);
+            startButtonRectTransform.sizeDelta = new Vector2(300, 100);
 
             ColorBlock startButtonColors;
             startButtonColors = startButtonGO.GetComponent<Button>().colors;
-            startButtonColors.normalColor = new Color(255,0,0);
-            startButtonColors.highlightedColor = new Color(0,255,0);
-            startButtonColors.pressedColor = new Color(0,0,255);
+            startButtonColors.normalColor = new Color(255, 0, 0);
+            startButtonColors.highlightedColor = new Color(0, 255, 0);
+            startButtonColors.pressedColor = new Color(0, 0, 255);
 
             Button startButton;
             startButton = startButtonGO.GetComponent<Button>();
@@ -131,28 +137,70 @@ public class TextController : MonoBehaviour
 
             RectTransform startButtonTextRectTransform;
             startButtonTextRectTransform = startButtonTextGO.GetComponent<RectTransform>();
-            startButtonTextRectTransform.anchorMin = new Vector2(0,0);
-            startButtonTextRectTransform.anchorMax = new Vector2(1,1);
+            startButtonTextRectTransform.anchorMin = new Vector2(0, 0);
+            startButtonTextRectTransform.anchorMax = new Vector2(1, 1);
             startButtonTextRectTransform.pivot = new Vector2(0.5f, 0.5f);
-            startButtonTextRectTransform.anchoredPosition = new Vector2(0,0);
+            startButtonTextRectTransform.anchoredPosition = new Vector2(0, 0);
 
             Text startButtonText;
             startButtonText = startButtonTextGO.GetComponent<Text>();
             startButtonText.text = "Start!";
-            startButtonText.font = arial;
-            startButtonText.fontSize = 60;
+            startButtonText.font = welcomeFont;
+            startButtonText.fontSize = welcomeFontSize;
             startButtonText.alignment = TextAnchor.MiddleCenter;
-            startButtonText.color = new Color(0,0,0);
+            startButtonText.color = new Color(0, 0, 0);
 
-            
+            //Create the 'Stickers!' button
+            GameObject stickersButtonGO = new GameObject();
+            stickersButtonGO.name = "StickersButton";
+            stickersButtonGO.transform.parent = canvasGO.transform;
+            stickersButtonGO.AddComponent<RectTransform>();
+            stickersButtonGO.AddComponent<Image>();
+            stickersButtonGO.AddComponent<Button>();
 
+            RectTransform stickersButtonRectTransform;
+            stickersButtonRectTransform = stickersButtonGO.GetComponent<RectTransform>();
+            stickersButtonRectTransform.localPosition = new Vector3(0, -170, 0);
+            stickersButtonRectTransform.sizeDelta = new Vector2(450, 100);
 
-        Debug.Log("Welcome to Shapes & Colors!");
-    }
+            ColorBlock stickersButtonColors;
+            stickersButtonColors = stickersButtonGO.GetComponent<Button>().colors;
+            stickersButtonColors.normalColor = new Color(0, 0, 255);
+            stickersButtonColors.highlightedColor = new Color(0, 255, 0);
+            stickersButtonColors.pressedColor = new Color(255, 0, 0);
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+            Button stickersButton;
+            stickersButton = stickersButtonGO.GetComponent<Button>();
+            stickersButton.targetGraphic = stickersButtonGO.GetComponent<Image>();
+            stickersButton.colors = stickersButtonColors;
+
+            //Create the 'Stickers!' text in the 'Stickers!' button
+            GameObject stickersButtonTextGO = new GameObject();
+            stickersButtonTextGO.name = "StickersButtonText";
+            stickersButtonTextGO.transform.parent = stickersButtonGO.transform;
+            stickersButtonTextGO.AddComponent<RectTransform>();
+            stickersButtonTextGO.AddComponent<Text>();
+
+            RectTransform stickersButtonTextRectTransform;
+            stickersButtonTextRectTransform = stickersButtonTextGO.GetComponent<RectTransform>();
+            stickersButtonTextRectTransform.anchorMin = new Vector2(0, 0);
+            stickersButtonTextRectTransform.anchorMax = new Vector2(1, 1);
+            stickersButtonTextRectTransform.pivot = new Vector2(0.5f, 0.5f);
+            stickersButtonTextRectTransform.anchoredPosition = new Vector2(0, 0);
+
+            Text stickersButtonText;
+            stickersButtonText = stickersButtonTextGO.GetComponent<Text>();
+            stickersButtonText.text = "Stickers!";
+            stickersButtonText.font = welcomeFont;
+            stickersButtonText.fontSize = welcomeFontSize;
+            stickersButtonText.alignment = TextAnchor.MiddleCenter;
+            stickersButtonText.color = new Color(0, 0, 0);
+
+            Debug.Log("Welcome to Shapes & Colors!");
+        }
+
+        void Update()
+        {
+        }
     }
 }
