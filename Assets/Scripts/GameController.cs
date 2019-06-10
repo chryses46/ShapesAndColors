a65bd90b1aec277270ed2eb16081f9a4f981b67e
Daniel <danjfrank08@gmail.com>
2019-06-10 07:36:24 -0500

    Created menu buttons
    menu buttons created to move between states
    Still need to work on enable/ disable of buttons when moving
    Still need to flesh out states and their usage

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -6,16 +7,125 @@ namespace ShapesAndColors{
 
     public class GameController : MonoBehaviour
     {
-        // Start is called before the first frame update
-        void Start()
+        TextController textController;
+        public enum _GameState
         {
+            MainMenu,
+            Play,
+            Stickers,
+            Paused
+
+        }
+        _GameState gameState = _GameState.MainMenu;
+
+        // Start is called before the first frame update
+        void Awake()
+        {
+            
             
         }
 
         // Update is called once per frame
-        void Update()
+        void Start()
         {
-            
+            textController = FindObjectOfType<TextController>();
         }
+
+        private void EnableDiableMainMenuObjects(_GameState gameState)
+        {
+
+            if( textController.mainMenuObjects == null){
+
+                Debug.Log("Menu objects not loaded yet.");
+
+            }else if(gameState == _GameState.MainMenu)
+            {
+                for(int i = 0; i < textController.mainMenuObjects.Count; i++)
+                {
+                    GameObject menuObject = textController.mainMenuObjects[i];
+                    menuObject.SetActive(true);
+                }
+
+            }else{
+                
+                for(int i = 0; i < textController.mainMenuObjects.Count; i++)
+                {
+                    GameObject menuObject = textController.mainMenuObjects[i];
+                    menuObject.SetActive(false);
+                }
+            }
+        }
+
+
+        private void EnableDisablePlayObjects(_GameState gameState)
+        {
+            if(gameState == _GameState.Play)
+            {
+                textController.mainMenuButtonGO.SetActive(true);
+                textController.pauseScreenButtonGO.SetActive(true);
+
+                for(int i = 0; i < textController.playObjects.Count; i++)
+                {
+                    GameObject playObject = textController.playObjects[i];
+                    playObject.SetActive(true);
+                }
+            }else{
+                for(int i = 0; i < textController.playObjects.Count; i++)
+                {
+                    GameObject playObject = textController.playObjects[i];
+                    playObject.SetActive(false);
+                }
+
+            }
+        }
+
+        public void PlayGame()
+        {
+            Debug.Log("GameState now in Play.");
+            gameState = _GameState.Play;
+            Debug.Log(gameState);
+
+            EnableDiableMainMenuObjects(gameState);
+            EnableDisablePlayObjects(gameState);
+
+        }
+
+        public void StickerBook()
+        {
+            Debug.Log("GameState now in Stickers.");
+            gameState = _GameState.Stickers;
+            Debug.Log(gameState);
+
+            EnableDiableMainMenuObjects(gameState);
+            EnableDisablePlayObjects(gameState);
+        }
+
+        public void MainMenu()
+        {
+            Debug.Log("GameState is now MainMenu.");
+            gameState = _GameState.MainMenu;
+            Debug.Log(gameState);
+
+            EnableDisablePlayObjects(gameState);
+            EnableDiableMainMenuObjects(gameState);
+
+        }
+
+        public void Pause(_GameState gamestate)
+        {
+            if(gameState == _GameState.Paused)
+            {
+                Debug.Log("GameState is now Paused.");
+                gameState = _GameState.Paused;
+                Debug.Log(gameState);
+
+                EnableDisablePlayObjects(gameState);
+                textController.pauseScreenGO.SetActive(true);
+            }
+            
+
+        }
+
+
     }
 }
--- Assets/Scripts/TextController.cs
+++ Assets/Scripts/TextController.cs
@@ -3,23 +3,43 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityStandardAssets;
 using UnityEngine.UI;
+using System;
 
 namespace ShapesAndColors{
     public class TextController : MonoBehaviour
     {
+        GameController gameController;
+        
         public Font font;
         
+        public Transform mainCanvas;
+        public List<GameObject> mainMenuObjects = new List<GameObject>();
+        public List<GameObject> playObjects = new List<GameObject>();
+        Button startButton;
+        Button stickersButton;
+        public GameObject mainMenuButtonGO;
+        public Button mainMenuButton;
+        public GameObject pauseScreenButtonGO;
+        public Button pauseScreenButton;
+        
+        public GameObject pauseScreenGO;
         void Awake()
         {
+            gameController = FindObjectOfType<GameController>();
+
             var welcomeFont = font;
             var welcomeFontSize = 100;
 
             //Create the UI Canvas
             GameObject canvasGO = new GameObject();
-            canvasGO.name = "Canvas";
+            
+            canvasGO.name = "MainCanvas";
             canvasGO.AddComponent<Canvas>();
             canvasGO.AddComponent<CanvasScaler>();
             canvasGO.AddComponent<GraphicRaycaster>();
+            
+            //Allow MainCanvas to be accessible outside of class.
+            mainCanvas = canvasGO.transform;
 
             Canvas canvas;
             canvas = canvasGO.GetComponent<Canvas>();
@@ -30,6 +50,8 @@ namespace ShapesAndColors{
             welcomeTextGO.name = "WelcomeText";
             welcomeTextGO.transform.parent = canvasGO.transform;
             welcomeTextGO.AddComponent<Text>();
+
+            mainMenuObjects.Add(welcomeTextGO);
 
             Text welcomeText;
             welcomeText = welcomeTextGO.GetComponent<Text>();
@@ -50,6 +72,8 @@ namespace ShapesAndColors{
             shapesTextGO.transform.parent = canvasGO.transform;
             shapesTextGO.AddComponent<Text>();
 
+            mainMenuObjects.Add(shapesTextGO);
+
             Text shapesText;
             shapesText = shapesTextGO.GetComponent<Text>();
             shapesText.text = "Shapes";
@@ -69,6 +93,8 @@ namespace ShapesAndColors{
             ampTextGO.transform.parent = canvasGO.transform;
             ampTextGO.AddComponent<Text>();
 
+            mainMenuObjects.Add(ampTextGO);
+
             Text ampText;
             ampText = ampTextGO.GetComponent<Text>();
             ampText.text = "&";
@@ -87,6 +113,8 @@ namespace ShapesAndColors{
             colorsTextGO.name = "ColorsText";
             colorsTextGO.transform.parent = canvasGO.transform;
             colorsTextGO.AddComponent<Text>();
+
+            mainMenuObjects.Add(colorsTextGO);
 
             Text colorsText;
             colorsText = colorsTextGO.GetComponent<Text>();
@@ -109,6 +137,9 @@ namespace ShapesAndColors{
             startButtonGO.AddComponent<Image>();
             startButtonGO.AddComponent<Button>();
 
+            mainMenuObjects.Add(startButtonGO);
+            startButton = startButtonGO.GetComponent<Button>();
+
             RectTransform startButtonRectTransform;
             startButtonRectTransform = startButtonGO.GetComponent<RectTransform>();
             startButtonRectTransform.localPosition = new Vector3(0, -1, 0);
@@ -120,8 +151,6 @@ namespace ShapesAndColors{
             startButtonColors.highlightedColor = new Color(0, 255, 0);
             startButtonColors.pressedColor = new Color(0, 0, 255);
 
-            Button startButton;
-            startButton = startButtonGO.GetComponent<Button>();
             startButton.targetGraphic = startButtonGO.GetComponent<Image>();
             startButton.colors = startButtonColors;
 
@@ -155,6 +184,9 @@ namespace ShapesAndColors{
             stickersButtonGO.AddComponent<Image>();
             stickersButtonGO.AddComponent<Button>();
 
+            mainMenuObjects.Add(stickersButtonGO);
+            stickersButton = stickersButtonGO.GetComponent<Button>();
+
             RectTransform stickersButtonRectTransform;
             stickersButtonRectTransform = stickersButtonGO.GetComponent<RectTransform>();
             stickersButtonRectTransform.localPosition = new Vector3(0, -170, 0);
@@ -166,8 +198,6 @@ namespace ShapesAndColors{
             stickersButtonColors.highlightedColor = new Color(0, 255, 0);
             stickersButtonColors.pressedColor = new Color(255, 0, 0);
 
-            Button stickersButton;
-            stickersButton = stickersButtonGO.GetComponent<Button>();
             stickersButton.targetGraphic = stickersButtonGO.GetComponent<Image>();
             stickersButton.colors = stickersButtonColors;
 
@@ -193,11 +223,177 @@ namespace ShapesAndColors{
             stickersButtonText.alignment = TextAnchor.MiddleCenter;
             stickersButtonText.color = new Color(0, 0, 0);
 
+            //Create the 'MainMenu' button
+            mainMenuButtonGO = new GameObject();
+            mainMenuButtonGO.name = "MainMenuButton";
+            mainMenuButtonGO.transform.parent = canvasGO.transform;
+            mainMenuButtonGO.AddComponent<RectTransform>();
+            mainMenuButtonGO.AddComponent<Image>();
+            mainMenuButtonGO.AddComponent<Button>();
+
+            mainMenuButtonGO.SetActive(false);
+
+            mainMenuButton = mainMenuButtonGO.GetComponent<Button>();
+
+            RectTransform mainMenuButtonRectTransform;
+            mainMenuButtonRectTransform = mainMenuButtonGO.GetComponent<RectTransform>();
+            mainMenuButtonRectTransform.localPosition = new Vector3(0, -170, 0);
+            mainMenuButtonRectTransform.sizeDelta = new Vector2(450, 100);
+
+            ColorBlock mainMenuButtonColors;
+            mainMenuButtonColors = mainMenuButtonGO.GetComponent<Button>().colors;
+            mainMenuButtonColors.normalColor = new Color(0, 0, 255);
+            mainMenuButtonColors.highlightedColor = new Color(0, 255, 0);
+            mainMenuButtonColors.pressedColor = new Color(255, 0, 0);
+
+            mainMenuButton.targetGraphic = mainMenuButtonGO.GetComponent<Image>();
+            mainMenuButton.colors = mainMenuButtonColors;
+
+            //Create the 'mainMenu!' text in the 'mainMenu!' button
+            GameObject mainMenuButtonTextGO = new GameObject();
+            mainMenuButtonTextGO.name = "MainMenuButtonText";
+            mainMenuButtonTextGO.transform.parent = mainMenuButtonGO.transform;
+            mainMenuButtonTextGO.AddComponent<RectTransform>();
+            mainMenuButtonTextGO.AddComponent<Text>();
+
+            RectTransform mainMenuButtonTextRectTransform;
+            mainMenuButtonTextRectTransform = mainMenuButtonTextGO.GetComponent<RectTransform>();
+            mainMenuButtonTextRectTransform.anchorMin = new Vector2(0, 0);
+            mainMenuButtonTextRectTransform.anchorMax = new Vector2(1, 1);
+            mainMenuButtonTextRectTransform.pivot = new Vector2(0.5f, 0.5f);
+            mainMenuButtonTextRectTransform.anchoredPosition = new Vector2(0, 0);
+
+            Text mainMenuButtonText;
+            mainMenuButtonText = mainMenuButtonTextGO.GetComponent<Text>();
+            mainMenuButtonText.text = "Main Menu";
+            mainMenuButtonText.font = welcomeFont;
+            mainMenuButtonText.fontSize = welcomeFontSize;
+            mainMenuButtonText.alignment = TextAnchor.MiddleCenter;
+            mainMenuButtonText.color = new Color(0, 0, 0);
+
+            //Create the 'PauseScreen' button
+            pauseScreenButtonGO = new GameObject();
+            pauseScreenButtonGO.name = "PauseScreenButton";
+            pauseScreenButtonGO.transform.parent = canvasGO.transform;
+            pauseScreenButtonGO.AddComponent<RectTransform>();
+            pauseScreenButtonGO.AddComponent<Image>();
+            pauseScreenButtonGO.AddComponent<Button>();
+
+            pauseScreenButtonGO.SetActive(false);
+
+            //pauseScreenObjects.Add(pauseScreenButtonGO);
+            pauseScreenButton = pauseScreenButtonGO.GetComponent<Button>();
+
+            RectTransform pauseScreenButtonRectTransform;
+            pauseScreenButtonRectTransform = pauseScreenButtonGO.GetComponent<RectTransform>();
+            float pauseOffsetMinX = pauseScreenButtonRectTransform.offsetMin.x;
+            pauseOffsetMinX = 1673.32f;
+            float pauseOffsetMaxX = pauseScreenButtonRectTransform.offsetMax.x;
+            pauseOffsetMaxX = 0;
+            float pauseOffsetMinY = pauseScreenButtonRectTransform.offsetMin.y;
+            pauseOffsetMinY  = 948f;
+            float pauseOffsetMaxY = pauseScreenButtonRectTransform.offsetMax.y;
+            pauseOffsetMaxY  = 0;
+
+            ColorBlock pauseScreenButtonColors;
+            pauseScreenButtonColors = pauseScreenButtonGO.GetComponent<Button>().colors;
+            pauseScreenButtonColors.normalColor = new Color(0, 0, 255);
+            pauseScreenButtonColors.highlightedColor = new Color(0, 255, 0);
+            pauseScreenButtonColors.pressedColor = new Color(255, 0, 0);
+
+            pauseScreenButton.targetGraphic = pauseScreenButtonGO.GetComponent<Image>();
+            pauseScreenButton.colors = pauseScreenButtonColors;
+
+            //Create the 'pauseScreen!' text in the 'pauseScreen!' button
+            GameObject pauseScreenButtonTextGO = new GameObject();
+            pauseScreenButtonTextGO.name = "PauseScreenButtonText";
+            pauseScreenButtonTextGO.transform.parent = pauseScreenButtonGO.transform;
+            pauseScreenButtonTextGO.AddComponent<RectTransform>();
+            pauseScreenButtonTextGO.AddComponent<Text>();
+
+            RectTransform pauseScreenButtonTextRectTransform;
+            pauseScreenButtonTextRectTransform = pauseScreenButtonTextGO.GetComponent<RectTransform>();
+            pauseScreenButtonTextRectTransform.anchorMin = new Vector2(0, 0);
+            pauseScreenButtonTextRectTransform.anchorMax = new Vector2(1, 1);
+            pauseScreenButtonTextRectTransform.pivot = new Vector2(0.5f, 0.5f);
+            pauseScreenButtonTextRectTransform.anchoredPosition = new Vector2(0, 0);
+
+            Text pauseScreenButtonText;
+            pauseScreenButtonText = pauseScreenButtonTextGO.GetComponent<Text>();
+            pauseScreenButtonText.text = "||";
+            pauseScreenButtonText.font = welcomeFont;
+            pauseScreenButtonText.fontSize = welcomeFontSize;
+            pauseScreenButtonText.alignment = TextAnchor.MiddleCenter;
+            pauseScreenButtonText.color = new Color(0, 0, 0);
+
+            //Create text boxes for colors and shapes
+            for(int i = 0; i < 3; i++)
+            {
+                GameObject textHolderGO = new GameObject();
+                textHolderGO.name = "TextHolder_" + i;
+                textHolderGO.transform.parent = canvasGO.transform;
+                textHolderGO.AddComponent<RectTransform>();
+                textHolderGO.AddComponent<Text>();
+                textHolderGO.SetActive(false);
+
+                playObjects.Add(textHolderGO);
+            }
+
+            pauseScreenGO = new GameObject();
+            pauseScreenGO.name = "PauseScreen";
+            pauseScreenGO.transform.parent = canvasGO.transform;
+            pauseScreenGO.SetActive(false);
+
+            //Create the 'PauseScreen' text
+            GameObject pauseScreenTextGO = new GameObject();
+            pauseScreenTextGO.name = "PauseScreenText";
+            pauseScreenTextGO.transform.parent = pauseScreenGO.transform;
+            pauseScreenTextGO.AddComponent<RectTransform>();
+            pauseScreenTextGO.AddComponent<Text>();
+
+            Text pauseScreenText;
+            pauseScreenText = pauseScreenTextGO.GetComponent<Text>();
+            pauseScreenText.text = "Game Paused";
+            pauseScreenText.font = welcomeFont;
+            pauseScreenText.fontSize = welcomeFontSize + 5;
+            pauseScreenText.color = new Color(255, 255, 0);
+            pauseScreenText.alignment = TextAnchor.MiddleCenter;
+
+
             Debug.Log("Welcome to Shapes & Colors!");
         }
 
-        void Update()
+        
+        void Start()
         {
+            startButton.onClick.AddListener(StartGame);
+            stickersButton.onClick.AddListener(GoToStickers);
+            mainMenuButton.onClick.AddListener(MainMenu);
+            pauseScreenButton.onClick.AddListener(Pause);
+
+        }
+
+        
+
+        void StartGame()
+        {
+            Debug.Log("Game Started!");
+            gameController.PlayGame();
+        }
+        void GoToStickers()
+        {
+            Debug.Log("Moving to sticker book.");
+            gameController.StickerBook();
+        }
+        void MainMenu()
+        {
+            Debug.Log("Moving to main menu.");
+            gameController.MainMenu();
+        }
+        private void Pause()
+        {
+            Debug.Log("Game is paused.");
+            gameController.Pause(GameController._GameState.Paused);
         }
     }
 }
